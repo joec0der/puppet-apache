4e06c316a2dc429adbdc2ca11a48be2f56e053d6
root <root@81539a8e6a1c.mylabserver.com>
2021-05-28 16:31:22 +0000

    Added cionfig class

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,15 @@
 #
 # @example
 #   include apache
-class apache {
-  include apache::install
+class apache (
+  String $install_name,
+  String $install_ensure,
+  String $config_ensure,
+  String $config_path,
+) {
+  contain apache::install
+  contain apache::config
+
+  Class['::apache::install']
+  -> Class['::apache::config']
 }
